b67226374f4c8088b444926a02d13f855b081833
Eagle3343 <Eagle3343>
2024-11-10 19:55:08 +0400

    code solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,33 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+
+function Calculator(num1,num2,operator){
+    if (operator ==="+" ) {
+       return num1+num2
+    } 
+    else if (operator ==="-" ) {
+       return num1-num2
+    } 
+    else if (operator ==="*" ) {
+       return num1*num2
+    } 
+    else if (operator ==="/" ) {
+       return num1/num2
+    } 
+    else if (operator ==="%" ) {
+       return "invalid Number"
+    } 
+    else {
+       
+    }
+   }
+   
+   console.log(Calculator(4,3,"+"));
+   console.log(Calculator(4,3,"-"));
+   console.log(Calculator(4,3,"*"));
+   console.log(Calculator(4,3,"/"));
+   console.log(Calculator(4,3,"*"));
+   console.log(Calculator(4,3,"%"));
+   
